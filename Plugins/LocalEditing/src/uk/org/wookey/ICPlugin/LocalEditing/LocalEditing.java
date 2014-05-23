a1f5edc4ecbbfc41a42f4aa38f004616ea7c3e04
znac049@gmail.com <znac049@gmail.com@546d2d4c-147f-2790-4b45-bd4d0817f54d>
2014-05-23 09:33:47 +0000

    Working towards a working Local Editing plugin.

--- Plugins/LocalEditing/src/uk/org/wookey/ICPlugin/LocalEditing/LocalEditing.java
+++ Plugins/LocalEditing/src/uk/org/wookey/ICPlugin/LocalEditing/LocalEditing.java
@@ -1,21 +1,108 @@
 package uk.org.wookey.ICPlugin.LocalEditing;
 
+import java.io.IOException;
+
+import javax.swing.JPanel;
+
+import uk.org.wookey.IC.MCP.MCPException;
+import uk.org.wookey.IC.Tabs.WorldTab;
 import uk.org.wookey.IC.Utils.Logger;
-import uk.org.wookey.IC.Utils.Plugin;;
+import uk.org.wookey.IC.Utils.Plugin;
+import uk.org.wookey.IC.Utils.StringParser;
 
 public class LocalEditing extends Plugin {
 	public final Logger _logger = new Logger("LocalEditing");
-	
+	private String outOfBandToken = "#$#";
+	private StringParser cmdParser = new StringParser("");
+
 	@Override
 	public boolean energizePlugin() {
+		setName("Local Editing");
 		_logger.logMsg("Hello from the local editing plugin");
 		
 		return true;
 	}
 	
 	@Override
-	public boolean handlesOOB() {
-		_logger.logMsg("handlesOOB(); -> true");
+	public boolean handlesRemoteLineInput() {
 		return true;
 	}
+	
+	@Override
+	public int handleRemoteLineInput(String line) {
+		String cmd;
+		String name = "";
+		String upload = "";
+		String bit;
+		
+		if (!line.startsWith(outOfBandToken)) {
+			return NotInterested;
+		}
+		
+		cmdParser.setString(line.trim());
+		
+		_logger.logMsg("Handle '" + line + "'");
+
+		try {
+			cmd = cmdParser.nextItem();
+			_logger.logMsg("Command is '" + cmd + "'");
+			
+			if (cmd.equalsIgnoreCase("edit")) {
+				String type = "moo-code";
+				String content = gobbleRemote();
+				
+				name = cmdParser.nextItem();
+				if (!name.equalsIgnoreCase("name:")) {
+					_logger.logMsg("Badly formatted OOB edit command");
+					return HandledFinal;
+				}
+				name = "";
+				bit = cmdParser.nextItem();
+				while (!bit.equalsIgnoreCase("upload:")) {
+					name += bit + " ";
+					bit = cmdParser.nextItem();
+				}
+				
+				name = name.trim();
+				upload = cmdParser.getRemainingLine().trim();
+				
+				_logger.logMsg("Name='" + name + "'");
+				_logger.logMsg("Upload='" + upload + "'");
+				_logger.logMsg("Type='" + type + "'");
+				_logger.logMsg("Content='" + content + "'");
+				
+				new LocalEditorForm(name, type, upload, content, _worldTab);
+
+				return HandledFinal;
+			}
+		} catch (MCPException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return NotInterested;
+	}
+	
+	private String gobbleRemote() {
+		String gobbled = "";
+		String line;
+		
+		try {
+			line = _worldTab.readRemote();
+			_logger.logMsg("Gobble: '" + line + "'");
+			
+			while (line.compareTo(".") != 0) {
+				gobbled += line + '\n';
+				line = _worldTab.readRemote();
+				_logger.logMsg("Gobble: '" + line + "'");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			
+			return gobbled;
+		}
+		
+		return gobbled;
+	}
+
 }
