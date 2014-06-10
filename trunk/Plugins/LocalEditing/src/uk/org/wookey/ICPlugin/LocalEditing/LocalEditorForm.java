72bf6a1949927f9d85428a8e2ba9f1c4f20ba9be
znac049@gmail.com <znac049@gmail.com@546d2d4c-147f-2790-4b45-bd4d0817f54d>
2014-06-10 22:40:13 +0000

    Modified to work with ServerPort instead of WorldTab

--- trunk/Plugins/LocalEditing/src/uk/org/wookey/ICPlugin/LocalEditing/LocalEditorForm.java
+++ trunk/Plugins/LocalEditing/src/uk/org/wookey/ICPlugin/LocalEditing/LocalEditorForm.java
@@ -4,18 +4,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import uk.org.wookey.IC.Editor.EditorForm;
-import uk.org.wookey.IC.Tabs.WorldTab;
+import uk.org.wookey.IC.GUI.WorldTab;
 import uk.org.wookey.IC.Utils.Logger;
+import uk.org.wookey.IC.Utils.ServerPort;
 
 public class LocalEditorForm extends EditorForm implements ActionListener {
 	private static final long serialVersionUID = 4644731315830687023L;
 	private final Logger _logger = new Logger("OOBEditorForm");
 	private String _upload;
+	private ServerPort server;
 
-	public LocalEditorForm(String name, String type, String upload, String content, WorldTab tab) {
-		super(name, type, content, tab);
+	public LocalEditorForm(String name, String type, String upload, String content, ServerPort svr) {
+		super(name, type, content);
 		
 		_upload = upload;
+		server = svr;
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -27,10 +30,10 @@ public class LocalEditorForm extends EditorForm implements ActionListener {
 		}
 		else if (cmd.equalsIgnoreCase("Save")) {
 			_logger.logMsg("Save back to MOO (OOB)!");
-			worldTab.writeRemote(_upload);
-			worldTab.writeRemote(_editor.getText());
-			worldTab.writeRemote(".");
-			worldTab.writeRemote(":blinks");
+			server.writeLine(_upload);
+			server.writeLine(_editor.getText());
+			server.writeLine(".");
+			//server.writeLine(":blinks");
 			setVisible(false);
 			dispose();
 		}
