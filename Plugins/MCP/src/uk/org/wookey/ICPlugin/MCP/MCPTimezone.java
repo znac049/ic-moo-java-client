701038813584d8eff5673628fab0e7f9332b4777
znac049@gmail.com <znac049@gmail.com@546d2d4c-147f-2790-4b45-bd4d0817f54d>
2014-06-13 21:44:48 +0000

    Closes issue 23

--- Plugins/MCP/src/uk/org/wookey/ICPlugin/MCP/MCPTimezone.java
+++ Plugins/MCP/src/uk/org/wookey/ICPlugin/MCP/MCPTimezone.java
@@ -1,10 +1,14 @@
 package uk.org.wookey.ICPlugin.MCP;
 
+import java.util.TimeZone;
+
 import uk.org.wookey.IC.Utils.Logger;
 import uk.org.wookey.IC.Utils.ParserException;
 import uk.org.wookey.IC.Utils.ServerPort;
 
 public class MCPTimezone extends MCPHandler implements Runnable {
+	Logger _logger = new Logger("MCPTimezone");
+	
 	public MCPTimezone(ServerPort svr, MCPRoot mcpRoot) throws ParserException {
 		super("dns-com-awns-timezone", "1.0", "1.0", svr, mcpRoot);
 	}
@@ -17,11 +21,15 @@ public class MCPTimezone extends MCPHandler implements Runnable {
 	}
 	
 	public void run() {
+		String tz = TimeZone.getDefault().getID();
+		
+		_logger.logInfo("TZ=" + tz);
+		
 		MCPCommand command = new MCPCommand();
 		command.setName(name);
 		
 		command.setAuthKey(mcp.authKey);
-		command.addParam("timezone", "UTC");
+		command.addParam("timezone", tz);
 		
 		command.sendToServer(server);
 	}
