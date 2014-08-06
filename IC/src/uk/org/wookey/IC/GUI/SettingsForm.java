bc81d69498beb41c98320777d58f22c0e6b74a28
znac049@gmail.com <znac049@gmail.com@546d2d4c-147f-2790-4b45-bd4d0817f54d>
2014-08-06 22:56:43 +0000

    General development (mostly of config screens).

--- IC/src/uk/org/wookey/IC/GUI/SettingsForm.java
+++ IC/src/uk/org/wookey/IC/GUI/SettingsForm.java
@@ -1,41 +1,69 @@
 package uk.org.wookey.IC.GUI;
 
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import uk.org.wookey.IC.Utils.CorePlugin;
+import uk.org.wookey.IC.Utils.CorePluginInterface.PluginType;
+import uk.org.wookey.IC.Utils.PluginManager;
 import webBoltOns.layoutManager.GridFlowLayout;
 import webBoltOns.layoutManager.GridFlowLayoutParameter;
 
-public class SettingsForm extends JFrame {
+public class SettingsForm extends JDialog {
 	private static final long serialVersionUID = 5212833746627586111L;
-	private JTextField pluginDir;
 
 	public SettingsForm() {
-		super("Settings");
+		super(new JFrame(), "Global Settings", true);
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		setLayout(new GridFlowLayout(4, 6));
+		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 		
-		pluginDir = new JTextField(20);
-		addComp("Plugin Directory", pluginDir);
+		ArrayList<CorePlugin> plugins = PluginManager.pluginsSupporting(PluginType.IOPLUGIN);
+		for (CorePlugin p: plugins) {
+			GlobalConfigPanel conf = p.getGlobalSettings();
+			
+			add(conf);
+		}
 		
-		setLocation(300, 300);
+		JPanel buttons = new JPanel();
+		
+		JButton okBtn = new JButton("OK");
+		buttons.add(okBtn);
+		
+		okBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				setVisible(false);
+			}
+		});
+		
+		JButton cancelBtn = new JButton("Cancel");
+		buttons.add(cancelBtn);
+		
+		cancelBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				setVisible(false);
+			}
+		});
+		
+		add(buttons);
+		
+		setLocation(300, 150);
+		setSize(500, 350);
 		pack();
 		setResizable(false);
 		setVisible(true);
-	}
-	
-	private void addComp(String label, Component comp) {
-		JLabel lab = new JLabel(label, JLabel.TRAILING);
-		add(lab, new GridFlowLayoutParameter(GridFlowLayoutParameter.NEXT_ROW, 1));
-
-		lab.setLabelFor(comp);
-		add(comp, new GridFlowLayoutParameter(GridFlowLayoutParameter.CURRENT_ROW, 2));		
-	}
-
-
+	}	
 }
