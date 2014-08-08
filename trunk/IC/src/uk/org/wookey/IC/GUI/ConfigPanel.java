39b714c57531757877f999792981bc69cd00e2bc
znac049@gmail.com <znac049@gmail.com@546d2d4c-147f-2790-4b45-bd4d0817f54d>
2014-08-08 22:28:17 +0000

    Slow progress on issue 32

--- trunk/IC/src/uk/org/wookey/IC/GUI/ConfigPanel.java
+++ trunk/IC/src/uk/org/wookey/IC/GUI/ConfigPanel.java
@@ -1,14 +1,19 @@
 package uk.org.wookey.IC.GUI;
 
-import java.awt.Component;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 
+import uk.org.wookey.IC.Utils.ConfigInterface;
+import uk.org.wookey.IC.Utils.Logger;
+
 public class ConfigPanel extends JPanel {
+	private static Logger _logger = new Logger("ConfigPanel");
 	private static final long serialVersionUID = 1L;
+	
+	private ConfigInterface configClass = null;
 
 	public ConfigPanel(String name) {
 		super();
@@ -27,5 +32,18 @@ public class ConfigPanel extends JPanel {
 	
 	public void addItem(JComponent comp) {
 		addItem("", comp);
+	}
+	
+	public void registerConfigHandler(ConfigInterface configClass) {
+		this.configClass = configClass;
+	}
+	
+	public boolean saveChanges() {
+		_logger.logInfo("Saving changes");
+		
+		if (configClass != null) {
+			configClass.saveGlobalConfig();
+		}
+		return true;
 	}
 }
